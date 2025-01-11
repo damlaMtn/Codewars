d4cd0726e49bdadc5ed35f6932bd83d23e19a667
Damla Metin Orak <damla91@gmail.com>
2025-01-11 14:59:59 +0200

    leetcode - construct k palindrome strings

---
+++ leetcode-ConstructKPalindromeStrings/constructkpalindromestrings/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public static class Solution
+{
+    static void Main(string[] args)
+    {
+        string s = "annabelle";
+        int k = 2;
+        CanConstruct(s, k);
+    }
+
+    public static bool CanConstruct(string s, int k)
+    {
+        if (k > s.Length)
+            return false;
+
+        int[] freq = new int[26];
+        foreach (char ch in s)
+        {
+            freq[ch - 'a']++;
+        }
+
+        int odd = 0;
+        foreach (int i in freq)
+            if ((i & 1) == 1)
+                odd++;
+        if (odd > k)
+            return false;
+
+        return true;
+    }
+}
